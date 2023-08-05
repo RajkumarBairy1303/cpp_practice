a6f40a4244893f8ecd991f5291290cd12d474a41
Rajkumar Bairy <raj.b409@gmail.com>
2023-08-05 21:12:23 +0530

    updated singlelinkedlist

--- Singlelinkedlist/Singlelinkedlist.cpp
+++ Singlelinkedlist/Singlelinkedlist.cpp
@@ -41,7 +41,114 @@ public:
 			temp1->m_next = temp;
 		}
 	}
+    void sortLinkedList()
+	{		
+		auto Merge = [&](Node* a, Node* b, auto&& Merge)->Node*
+		{
+			if (a == nullptr)
+				return b;
+			if (b == nullptr)
+				return a;
 
+			Node* c;
+			if (a->m_data < b->m_data)
+			{
+				c = a;
+				c->m_next = Merge(a->m_next, b,Merge);
+			}
+			else
+			{
+				c = b;
+				c->m_next = Merge(a, b->m_next,Merge);
+			}
+
+			return c;
+		};
+		auto mid_point = [&](Node* head)
+		{
+			if (head == nullptr || head->m_next == nullptr)
+				return head;
+
+			Node* fast = head;
+			Node* slow = head;
+
+			while (fast != nullptr && fast->m_next != nullptr)
+			{
+				fast = fast->m_next;
+				if (fast->m_next == nullptr)
+					break;
+
+				fast = fast->m_next;
+				slow = slow->m_next;
+			}
+			return slow;
+		};
+		auto Merge_sort = [&](Node *head,auto&& Merge_sort)->Node*
+		{
+
+			if (head == nullptr || head->m_next == nullptr)
+				return head;
+
+			Node* temp = head;
+			Node *mid = mid_point(temp);
+
+			Node* a = head;
+			Node* b = mid->m_next;
+
+			mid->m_next = nullptr;
+
+			a = Merge_sort(a, Merge_sort);
+			b = Merge_sort(b, Merge_sort);
+
+			Node *c = Merge(a, b, Merge);
+
+			return c;
+
+		};
+		m_head = Merge_sort(m_head,Merge_sort);		
+	}
+	void reverseList()
+	{
+		if (m_head == nullptr)
+			return;
+
+		Node* reverse = nullptr;
+		while (m_head != nullptr)
+		{
+			Node *node = m_head;
+			m_head = m_head->m_next;
+			node->m_next = reverse;
+			reverse = node;
+		}
+		m_head = reverse;
+	}
+	void removeNode(int val)
+	{
+		Node* temp = m_head;
+		Node *del = nullptr;
+		if (m_head->m_data == val)
+		{
+			del = m_head;
+			m_head = m_head->m_next;
+		}
+		else
+		{
+			while (temp->m_next != nullptr && temp->m_next->m_data != val)
+				temp = temp->m_next;
+
+			if (temp->m_next != nullptr)
+			{
+				del = temp->m_next;
+				temp->m_next = temp->m_next->m_next;
+			}			
+		}
+
+		if (del != nullptr)
+		{
+			del->m_next = nullptr;
+			delete del;
+		}
+	}
 	void display()
 	{
 		Node* temp = m_head;
@@ -52,14 +159,67 @@ public:
 		}
 		std::cout << std::endl;
 	}
+	void reverseparticularlist(int start, int end)
+	{
+		Node* temp    = m_head;
+		int count     = 0;
+		Node* reverse = nullptr;
+		Node* prev    = nullptr;
+
+		while (temp != nullptr && count < start)
+		{		
+			prev = temp;
+			temp = temp->m_next;
+			count++;
+		}
+
+		while (temp != nullptr && count < end)
+		{
+			Node* rev = temp;
+			temp = temp->m_next;
+			rev->m_next = reverse;
+			reverse = rev;
+			count++;
+		}
+
+		if (prev == nullptr)
+		{
+			prev = reverse;
+			m_head = prev;
+		}
+		else
+		{
+			prev->m_next = reverse;
+		}
+			
+
+		while (reverse->m_next != nullptr)
+			reverse = reverse->m_next;
+
+		reverse->m_next = temp;		
+	}
 };
 int main()
 {
 	Singlelinkedlist list;
-	list.add(10);
+	list.add(50);
 	list.add(20);
+	list.add(10);
+	list.add(60);
+	list.display();
+	list.removeNode(10);
+	list.display();
+	list.reverseList();	
+	list.display();
+	list.add(15);
+	list.add(25);
 	list.add(30);
-	list.add(40);
+	list.add(80);
+	list.add(5);
+	list.display();
+	list.sortLinkedList();
+	list.display();
+	list.reverseparticularlist(3, 7);
 	list.display();
 	return 0;
 }
